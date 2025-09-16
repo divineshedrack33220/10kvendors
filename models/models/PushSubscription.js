0beb13f07ab9337d35511836cf7a1c9ea8f0860e
divine <123784238+divineshedrack33220@users.noreply.github.com>
2025-09-16 06:20:37 +0100

    Delete models directory

--- models/models/PushSubscription.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const pushSubscriptionSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  subscription: { type: Object, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('PushSubscription', pushSubscriptionSchema);
